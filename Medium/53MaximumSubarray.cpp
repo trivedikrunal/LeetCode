f15d8fc0af5ad3325c2ae1277a60f57c868f4955
Krunal Trivedi <137049008+trivedikrunal@users.noreply.github.com>
2024-09-15 23:10:17 +0530

    Medium

---
+++ Medium/53MaximumSubarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum = 0;
+        int maxSum = INT_MIN;
+        for(int val: nums){
+            currentSum += val;
+            maxSum = max(currentSum, maxSum);
+            if(currentSum < 0){
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
